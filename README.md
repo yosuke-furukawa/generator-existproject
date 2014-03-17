9d54ca7adbcfa69a949bf069b453c0024858aaad
Yosuke Furukawa <furukawa.yosuke@dena.jp>
2014-03-17 14:03:20 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# generator-existproject
+
+> Yeoman generator for Exisiting project (WIP)
+
+I would like to create scaffolds, but the project seed has been created. So I just don't wanna copy these project files to generator/app/templates manually.
+
+## Getting started
+
+- Install: `npm install -g generator-existproject`
+- Run: `yo existproject`
+
+## License
+
+MIT
---
+++ app/index.js
@@ -0,0 +1,142 @@
+'use strict';
+var path = require('path');
+var url = require('url');
+var yeoman = require('yeoman-generator');
+var chalk = require('chalk');
+var fs = require('fs');
+
+
+/* jshint -W106 */
+var proxy = process.env.http_proxy || process.env.HTTP_PROXY || process.env.https_proxy || process.env.HTTPS_PROXY || null;
+/* jshint +W106 */
+var githubOptions = {
+  version: '3.0.0'
+};
+
+if (proxy) {
+  var proxyUrl = url.parse(proxy);
+  githubOptions.proxy = {
+    host: proxyUrl.hostname,
+    port: proxyUrl.port
+  };
+}
+
+var GitHubApi = require('github');
+var github = new GitHubApi(githubOptions);
+
+var extractGeneratorName = function (_, appname) {
+  var slugged = _.slugify(appname);
+  var match = slugged.match(/^generator-(.+)/);
+
+  if (match && match.length === 2) {
+    return match[1].toLowerCase();
+  }
+
+  return slugged;
+};
+
+var githubUserInfo = function (name, cb) {
+  github.user.getFrom({
+    user: name
+  }, function (err, res) {
+    if (err) {
+      throw new Error(err.message + '\n\nCannot fetch your github profile. Make sure you\'ve typed it correctly.');
+    }
+    cb(JSON.parse(JSON.stringify(res)));
+  });
+};
+
+var GeneratorExistProject = yeoman.generators.Base.extend({
+  init: function () {
+    this.pkg = require('../package.json');
+    this.currentYear = (new Date()).getFullYear();
+
+    this.on('end', function () {
+      if (!this.options['skip-install']) {
+        this.npmInstall();
+      }
+    });
+  },
+
+  askFor: function () {
+    var done = this.async();
+    var generatorName = extractGeneratorName(this._, this.appname);
+    // have Yeoman greet the user
+    this.log(this.yeoman);
+    this.log(chalk.magenta('Create your own magical generator with superpowers!'));
+
+
+    var prompts = [{
+      name: 'githubUser',
+      message: 'Would you mind telling me your username on GitHub?',
+      default: 'someuser'
+    }, {
+      name: 'generatorName',
+      message: 'What\'s the base name of your generator?',
+      default: generatorName
+    }, {
+      name: 'projectPath',
+      message: 'What\'s the your existing project path?',
+      default: "."
+    }];
+
+    this.prompt(prompts, function (props) {
+      this.githubUser = props.githubUser;
+      this.generatorName = props.generatorName;
+      this.projectPath = props.projectPath;
+
+      this.appname = 'generator-' + this.generatorName;
+      done();
+    }.bind(this));
+  },
+
+  enforceFolderName: function () {
+    if (this.appname !== this._.last(this.destinationRoot().split(path.sep))) {
+      this.destinationRoot(this.appname);
+    }
+  },
+
+  userInfo: function () {
+    var done = this.async();
+
+    githubUserInfo(this.githubUser, function (res) {
+      /*jshint camelcase:false */
+      this.realname = res.name;
+      this.email = res.email;
+      this.githubUrl = res.html_url;
+      done();
+    }.bind(this));
+  },
+
+  projectfiles: function () {
+    this.template('_package.json', 'package.json');
+    this.template('editorconfig', '.editorconfig');
+    this.template('jshintrc', '.jshintrc');
+    this.template('_travis.yml', '.travis.yml');
+    this.template('README.md');
+  },
+
+  gitfiles: function () {
+    this.copy('gitattributes', '.gitattributes');
+    this.copy('gitignore', '.gitignore');
+  },
+
+  app: function () {
+    this.mkdir('app');
+    this.template('app/index.js');
+  },
+
+  templates: function () {
+    console.log(this.projectPath);
+    console.log(path.resolve(this.projectPath));
+    this.directory(path.resolve(this.projectPath), "app/templates");
+  },
+
+  tests: function () {
+    this.mkdir('test');
+    this.template('test-load.js', 'test/test-load.js');
+    this.template('test-creation.js', 'test/test-creation.js');
+  }
+});
+
+module.exports = GeneratorExistProject;
